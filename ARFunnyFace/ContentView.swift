70daeac35f620e9e5bcbd3b408543bba730ccf37
systemsevenfive <ryanwsaunders@icloud.com>
2023-01-22 22:19:20 -0500

    It's alive!

--- ARFunnyFace/ContentView.swift
+++ ARFunnyFace/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 import RealityKit
 
 var arView: ARView!
+var robot: Experience.Robot!
 
 struct ContentView : View {
     @State var propID: Int = 0
@@ -40,7 +41,7 @@ struct ContentView : View {
                 }
                 Spacer()
                 Button(action: {
-                    self.propID = self.propID >= 2 ? 2 : self.propID + 1
+                    self.propID = self.propID >= 3 ? 3 : self.propID + 1
                 }) {
                     Image("NextButton").clipShape(Circle())
                 }
@@ -51,10 +52,12 @@ struct ContentView : View {
 }
 
 struct ARViewContainer: UIViewRepresentable {
+  
     @Binding var propID: Int
     func makeUIView(context: Context) -> ARView {
         
        arView = ARView(frame: .zero)
+        arView.session.delegate = context.coordinator
 //
 //        // Load the "Box" scene from the "Experience" Reality File
 //        let boxAnchor = try! Experience.loadBox()
@@ -67,6 +70,8 @@ struct ARViewContainer: UIViewRepresentable {
     }
     
     func updateUIView(_ uiView: ARView, context: Context) {
+        arView.scene.anchors.removeAll()
+        robot = nil
         let arConfiguration = ARFaceTrackingConfiguration()
         uiView.session.run(arConfiguration, options: [.resetTracking, .removeExistingAnchors])
         
@@ -86,9 +91,77 @@ struct ARViewContainer: UIViewRepresentable {
             uiView.scene.anchors.append(arAnchor)
             break
             
+        case 3: // Robot
+            let arAnchor = try! Experience.loadRobot()
+            uiView.scene.anchors.append(arAnchor)
+            robot = arAnchor
+            break
+            
         default:
             break
         }
+    }
+    class ARDelegateHandler: NSObject, ARSessionDelegate {
+        var arViewContainer: ARViewContainer
+        var isLasersDone = true
+        init(_ control: ARViewContainer) {
+            arViewContainer = control
+            super.init()
+        }
+        
+        func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
+            guard robot != nil else { return }
+            var faceAnchor: ARFaceAnchor?
+            for anchor in anchors {
+                if let a = anchor as? ARFaceAnchor {
+                    faceAnchor = a
+                }
+            }
+            let blendShapes = faceAnchor?.blendShapes
+            let eyeBlinkLeft = blendShapes?[.eyeWideLeft]?.floatValue
+            let eyeBlinkRight = blendShapes?[.eyeWideRight]?.floatValue
+            let browInnerUp = blendShapes?[.browInnerUp]?.floatValue
+            let browLeft  = blendShapes?[.browDownLeft]?.floatValue
+            let browRight = blendShapes?[.browDownRight]?.floatValue
+            let jawOpen = blendShapes?[.jawOpen]?.floatValue
+            
+            // 1
+            robot.eyeLidL?.orientation = simd_mul(
+                // 2
+                simd_quatf(angle: Deg2Rad(-120 + (90 * eyeBlinkLeft!)),
+                           axis: [1, 0, 0]),
+                // 3
+                simd_quatf(angle: Deg2Rad((90 * browLeft!) - (30 * browInnerUp!)),
+                           axis: [0, 0, 1]))
+            // 4
+            robot.eyeLidR?.orientation = simd_mul(
+                simd_quatf(
+                    angle: Deg2Rad(-120 + (90 * eyeBlinkRight!)),
+                    axis: [1, 0, 0]),
+                simd_quatf(
+                    angle: Deg2Rad((-90 * browRight!) - (-30 * browInnerUp!)),
+                    axis: [0, 0, 1]))
+            
+            robot.jaw?.orientation = simd_quatf(
+                angle: Deg2Rad(-100 + (60 * jawOpen!)),
+                axis: [1, 0, 0])
+            
+            if (self.isLasersDone == true && jawOpen! > 0.9) {
+                self.isLasersDone = false
+                robot.notifications.showLasers.post()
+                robot.actions.lasersDone.onAction = { _ in
+                    self.isLasersDone = true
+                }
+            }
+        }
+        
+        func Deg2Rad(_ value: Float) -> Float {
+            return value * .pi / 180
+        }
+    }
+    
+    func makeCoordinator() -> ARDelegateHandler {
+        ARDelegateHandler(self)
     }
     
 }
